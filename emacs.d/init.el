125ddf69ce477dd84ffc6693fee87c47a7a49575
Trynity Mirell <trynity@mirell.org>
2013-12-24 02:32:53 -0600

    emacs.d/init.el: No longer required with newer versions of emacs

--- emacs.d/init.el
+++
@@ -1,6 +0,0 @@
-(require 'package)
-(add-to-list 'package-archives
-	                  '("marmalade" . "http://marmalade-repo.org/packages/") t)
-(add-to-list 'package-archives
-             '("tromey" . "http://tromey.com/elpa/") t)
-(package-initialize)
